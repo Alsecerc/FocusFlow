43b78f1b73b1fc68cd632d34b53623047bedf8ea
Heng Jun Kai <hengjunkai@gmail.com>
2025-02-11 22:15:57 +0800

    Calendar Page JS

--- RegisterLayout/Registered.js
+++ RegisterLayout/Registered.js
@@ -8,6 +8,14 @@ function getClass(element) {
     return document.getElementsByClassName(element);
 }
 
+function getQuery(element) {
+    return document.querySelector(element);
+}
+
+function getQueryAll(element) {
+    return document.querySelectorAll(element);
+}
+
 
 // TODO: HOMEPAGE
 var SIDEBAR = getClass("SIDEBAR")[0];
@@ -75,37 +83,150 @@ if (taskButton) {
 // TODO: Calendar Page
 // Calendar
 // Toggle function as webpage load
+// window.onload = function () {
+//     togglePeriod('week');
+// };
+
+// function togglePeriod(Options) {
+//     // Button Effect
+//     let ButtonList = Array.from(getClass("CALENDAR__HEADER__BUTTON"));
+//     ButtonList.forEach((buts) => {
+//         // remove select effect from all button
+//         buts.classList.remove("SELECTED_BUTTON");
+//     });
+
+//     let Button = getID(Options + "Button");
+//     Button.classList.add("SELECTED_BUTTON");
+
+
+//     // Content Display
+//     let ContentList = Array.from(getClass("CALENDAR__CONTENT__ITEM"));
+//     ContentList.forEach((conts) => {
+//         conts.classList.remove("CALENDAR__CONTENT_SHOW");
+//     });
+
+//     // Show the selected content div
+//     let Content = getID(Options + "Content");
+//     Content.classList.add("CALENDAR__CONTENT_SHOW");
+
+
+//     // Change title
+//     let Title = getClass("CALENDAR__TITLE")[0];
+//     Title.innerHTML = Options.toUpperCase();
+// };
+
+
+
+
+// get date time
+const TimeNow = new Date();
+const monthList = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const year = TimeNow.getFullYear();
+const month = TimeNow.getMonth();    // 1-12 (add 1 because months are 0-based)
+const day = TimeNow.getDate();
+const dayName = TimeNow.getDay();
+
+let CALENDAR__TITLE1 = getQuery("#calendar__title1");
+let DAYNUM_LIST = Array.from(getQueryAll('.DAY_NUM li'));
+// as page load the function will be run
+let DayOffset = day;
+let MonthOffset = month;
+let YearOffset = year;
+
 window.onload = function () {
-    togglePeriod('week');
-};
-
-function togglePeriod(Options) {
-    // Button Effect
-    let ButtonList = Array.from(getClass("CALENDAR__HEADER__BUTTON"));
-    ButtonList.forEach((buts) => {
-        // remove select effect from all button
-        buts.classList.remove("SELECTED_BUTTON");
+    switch (dayName) {
+        case 0:
+            DayOffset
+            break;
+        case 1:
+            DayOffset -= 1
+            break;
+        case 2:
+            DayOffset -= 2
+            break;
+        case 3:
+            DayOffset -= 3
+            break;
+        case 4:
+            DayOffset -= 4
+            break;
+        case 5:
+            DayOffset -= 5
+            break;
+        case 6:
+            DayOffset -= 6
+            break;
+    }
+    DAYNUM_LIST.forEach((Item) => {
+        // find out date of month
+        let totalDaysInMonth = new Date(year, MonthOffset + 1, 0).getDate();
+        if (DayOffset < totalDaysInMonth) {
+            Item.innerHTML = `${DayOffset}`
+            DayOffset += 1;
+        } else {
+            // reset the date num with new month
+            MonthOffset += 1;
+            DayOffset = 1;
+            Item.innerHTML = `${DayOffset}`
+        }
     });
 
-    let Button = getID(Options + "Button");
-    Button.classList.add("SELECTED_BUTTON");
+    CALENDAR__TITLE1.innerHTML = `${monthList[MonthOffset]}`
+}
 
 
-    // Content Display
-    let ContentList = Array.from(getClass("CALENDAR__CONTENT__ITEM"));
-    ContentList.forEach((conts) => {
-        conts.classList.remove("CALENDAR__CONTENT_SHOW");
+
+
+function toggleViewPrevious() {
+    DayOffset -= 14;
+
+    while (DayOffset <= 0) {
+        // go back 1 month
+        MonthOffset -= 1
+        if (MonthOffset < 0) {
+            // go back previous year December
+            MonthOffset = 11;
+            YearOffset -= 1;
+        }
+        let totalDaysInMonth = new Date(YearOffset, MonthOffset + 1, 0).getDate();
+        DayOffset += totalDaysInMonth;
+    }
+
+
+    DAYNUM_LIST.forEach((Item) => {
+        Item.innerHTML = `${DayOffset}`
+        DayOffset += 1;
     });
 
-    // Show the selected content div
-    let Content = getID(Options + "Content");
-    Content.classList.add("CALENDAR__CONTENT_SHOW");
+    CALENDAR__TITLE1.innerHTML = `${monthList[MonthOffset]}`
+}
+
+function toggleViewNext() {
+    let totalDaysInMonth = new Date(YearOffset, MonthOffset + 1, 0).getDate();
+    while (DayOffset >= totalDaysInMonth) {
+        MonthOffset += 1
+        if (MonthOffset > 11) {
+            // go back previous year December
+            MonthOffset = 1;
+            YearOffset += 1;
+        }
+        let totalDaysInMonth = new Date(YearOffset, MonthOffset + 1, 0).getDate();
+        DayOffset = Math.abs(totalDaysInMonth - DayOffset - 1);
+    }
+
+    DAYNUM_LIST.forEach((Item) => {
+        Item.innerHTML = `${DayOffset}`
+        DayOffset += 1;
+    });
+
+    CALENDAR__TITLE1.innerHTML = `${monthList[MonthOffset]}`
+}
 
 
-    // Change title
-    let Title = getClass("CALENDAR__TITLE")[0];
-    Title.innerHTML = Options.toUpperCase();
-};
 
 
 
