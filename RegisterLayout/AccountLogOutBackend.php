a07887d0e7a1757690002ffa6f2c90fce54965d9
Chen <cty889977@gmail.com>
2025-02-24 14:17:55 +0800

    Added destroy cookie

--- RegisterLayout/AccountLogOutBackend.php
+++ RegisterLayout/AccountLogOutBackend.php
@@ -3,6 +3,11 @@ session_start();
 session_unset(); // Unset all session variables
 session_destroy(); // Destroy the session
 
+setcookie('UID', '', time() - 86400, '/');
+setcookie("USERNAME", '', time() + 86400, '/');
+setcookie("EMAIL", '', time() - 86400, '/');
+setcookie("PASSWORD", '', time() - 86400, '/');
+setcookie("USERTYPE", '', time() - 86400, '/');
 // Redirect to login or homepage
 header("Location: Login.php"); 
 exit();
