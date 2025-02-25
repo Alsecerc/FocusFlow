f35497ee0fd870be5289c1600f0ff6fd6b391552
Chen <cty889977@gmail.com>
2025-02-25 20:58:35 +0800

    edited todo page

--- RegisterLayout/TodoBackend.php
+++ RegisterLayout/TodoBackend.php
@@ -2,50 +2,50 @@
     session_start();
     include "conn.php";
     // update data to database
-    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $userId = $_COOKIE['UID'];
-        if (isset($_POST['GROUPNAMECHOICE']) && isset($_POST['USERTASK'])){ // update when submit task form
-            $groupchoice = $_POST['GROUPNAMECHOICE'][0];
-            $taskContent = $_POST['USERTASK'];
-            $groupchoice = htmlspecialchars($groupchoice, ENT_QUOTES, 'UTF-8');
-            $taskContent = htmlspecialchars($taskContent, ENT_QUOTES, 'UTF-8');
-            $taskTitle = $groupchoice[0];
-            $taskDesc = $taskContent;
-            $startDate = date('Y-m-d');
-            $start_time = date('H:i:s');
-            $endTime = (isset($_POST['END__TIME']) && !empty($_POST['END__TIME'])) ? $_POST['END__TIME'] : date('H:i:s', strtotime('+1 hour'));  // add 1 hour by default
-            $sql = "INSERT INTO tasks (task_title, task_desc, start_date, start_time, end_time, user_id) VALUES (?, ?, ?, ?, ?, ?)";
-
-            if (rowExistance($userId, $groupchoice, $_conn)){
-                sqlInsertion($_conn, $sql, 'sssssi', $taskTitle, $taskDesc, $startDate, $start_time, $endTime, $userId);
-            }else{
-                echo"Row does not exists";
-                $_conn->close();
-            }
+    // if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    //     $userId = $_COOKIE['UID'];
+    //     if (isset($_POST['GROUPNAMECHOICE']) && isset($_POST['USERTASK'])){ // update when submit task form
+    //         $groupchoice = $_POST['GROUPNAMECHOICE'][0];
+    //         $taskContent = $_POST['USERTASK'];
+    //         $groupchoice = htmlspecialchars($groupchoice, ENT_QUOTES, 'UTF-8');
+    //         $taskContent = htmlspecialchars($taskContent, ENT_QUOTES, 'UTF-8');
+    //         $taskTitle = $groupchoice[0];
+    //         $taskDesc = $taskContent;
+    //         $startDate = date('Y-m-d');
+    //         $start_time = date('H:i:s');
+    //         $endTime = (isset($_POST['END__TIME']) && !empty($_POST['END__TIME'])) ? $_POST['END__TIME'] : date('H:i:s', strtotime('+1 hour'));  // add 1 hour by default
+    //         $sql = "INSERT INTO tasks (task_title, task_desc, start_date, start_time, end_time, user_id) VALUES (?, ?, ?, ?, ?, ?)";
+            
+    //         if (rowExistance($userId, $groupchoice, $_conn)){
+    //             sqlInsertion($_conn, $sql, 'sssssi', $taskTitle, $taskDesc, $startDate, $start_time, $endTime, $userId);
+    //         }else{
+    //             echo"Row does not exists";
+    //             $_conn->close();
+    //         }
     
-            echo "user id:". $userId. "<br>";
-            echo "Selected Group: " . $groupchoice . "<br>";
-            echo "Task Content: " . $taskContent . "<br>";
+    //         echo "user id:". $userId. "<br>";
+    //         echo "Selected Group: " . $groupchoice . "<br>";
+    //         echo "Task Content: " . $taskContent . "<br>";
 
-        }else if (isset($_POST['GROUPNAME'])) {
-            // update when submit group form
-            date_default_timezone_set('Asia/Kuala_Lumpur');
+    //     }else if (isset($_POST['GROUPNAME'])) {
+    //         // update when submit group form
+    //         date_default_timezone_set('Asia/Kuala_Lumpur');
 
-            $taskTitle = $_POST['GROUPNAME'];
-            $taskTitle = htmlspecialchars($taskTitle, ENT_QUOTES, 'UTF-8');
+    //         $taskTitle = $_POST['GROUPNAME'];
+    //         $taskTitle = htmlspecialchars($taskTitle, ENT_QUOTES, 'UTF-8');
 
-            $sql = "INSERT INTO tasks (task_title, user_id) VALUES (?, ?)";
+    //         $sql = "INSERT INTO tasks (task_title, user_id) VALUES (?, ?)";
 
-            sqlInsertion($_conn, $sql, 'si',$taskTitle, $userId);
+    //         sqlInsertion($_conn, $sql, 'si',$taskTitle, $userId);
 
-            echo "Group name: " . $taskTitle . "<br>";
-        }
-        else{
-            echo "missing form data";
-        }
-    }else{
-        echo "Request method:" . $_SERVER['REQUEST_METHOD']. "<br>";
-    }
+    //         echo "Group name: " . $taskTitle . "<br>";
+    //     }
+    //     else{
+    //         echo "missing form data";
+    //     }
+    // }else{
+    //     echo "Request method:" . $_SERVER['REQUEST_METHOD']. "<br>";
+    // }
     header('Content-Type: application/json');
     $data = ["name" => "Alice", "age" => 30];
     json_encode($data);
